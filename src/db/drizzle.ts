e137323b8c63450419c0102db702a730ab72fd5a
Nishant <nkmysign1990@gmail.com>
2025-08-13 14:25:00 +0530

    fixed drizzle

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,25 +1,4 @@
-import { schema } from './schema';
-import { config } from "dotenv";
 import { drizzle } from 'drizzle-orm/neon-http';
+import { schema } from './schema';
 
-config({ path: ".env" });
-
-function sanitizeDatabaseUrl(rawUrl: string): string {
-    let adjustedUrl = rawUrl.replace(/^postgresql:\/\//, "postgres://");
-    try {
-        const url = new URL(adjustedUrl);
-        url.searchParams.delete("channel_binding");
-        return url.toString();
-    } catch {
-        return adjustedUrl;
-    }
-}
-
-const rawDatabaseUrl = process.env.DATABASE_URL;
-if (!rawDatabaseUrl) {
-    throw new Error("DATABASE_URL is not set");
-}
-
-const databaseUrl = sanitizeDatabaseUrl(rawDatabaseUrl);
-
-export const db = drizzle(databaseUrl, { schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
